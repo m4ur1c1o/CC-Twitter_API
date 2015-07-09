fa1e1e1decc49cb49adb44510c3773c8865253ac
codeamac02 <codeamac02@gmail.com>
2015-07-09 14:16:29 -0500

    update migration, added user tokens for authentification

--- db/migrate/20150707195139_create_users.rb
+++ db/migrate/20150707195139_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration
    def change
   	create_table :twitter_users do |t|
   		t.string :username
+  		t.string :oauth_token
+  		t.string :oauth_token_secret
 
   		t.timestamps
   	end
