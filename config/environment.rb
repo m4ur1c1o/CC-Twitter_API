ac86065e0108eddc4978d452674189e1e53b6bdf
codeamac02 <codeamac02@gmail.com>
2015-07-09 14:10:16 -0500

    Defines Twitter environment variables

--- config/environment.rb
+++ config/environment.rb
@@ -46,11 +46,11 @@ end
 
 # Configura twitter API
 
-# $client = Twitter::REST::Client.new do |config|
-#   config.consumer_key        = yaml_file["CONSUMER_KEY"]
-#   config.consumer_secret     = yaml_file["CONSUMER_SECRET"]
-#   config.access_token        = yaml_file["TWITTER_TOKEN"]
-#   config.access_token_secret = yaml_file["TWITTER_SECRET"]
+# CLIENT = Twitter::REST::Client.new do |config|
+#   config.consumer_key        = ENV["CONSUMER_KEY"]
+#   config.consumer_secret     = ENV["CONSUMER_SECRET"]
+#   config.access_token        = ENV["TWITTER_TOKEN"]
+#   config.access_token_secret = ENV["TWITTER_SECRET"]
 # end
 
 CLIENT = Twitter::REST::Client.new do |config|
