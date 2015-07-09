084ece21e06974fe459e52d63fe53dac820a5a2d
codeamac02 <codeamac02@gmail.com>
2015-07-09 18:33:13 -0500

    Add tweet_later method

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -23,4 +23,13 @@ class TwitterUser < ActiveRecord::Base
     client.update!(text)
 
   end
+
+  def tweet_later(text)
+    # tweet = # Crea un tweet relacionado con este usuario en la tabla de tweets
+    # user_id = TwitterUser.find
+    tweet = Tweet.create(tweet: text, twitter_user_id: self.id)
+    # Este es un método de Sidekiq con el cual se agrega a la cola una tarea para ser
+    #La última linea debe de regresar un sidekiq job id
+    TweetWorker.perform_async(tweet.id)
+  end
 end
