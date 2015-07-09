06086ab9e11e33e6f88fa61cff9162d692cade01
codeamac02 <codeamac02@gmail.com>
2015-07-09 14:13:40 -0500

    update tweet method

--- app/models/twitter_user.rb
+++ app/models/twitter_user.rb
@@ -11,4 +11,16 @@ class TwitterUser < ActiveRecord::Base
   		false
   	end
   end
+
+  def tweet(text)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_KEY']
+      config.consumer_secret     = ENV['TWITTER_SECRET']
+      config.access_token        = self.oauth_token
+      config.access_token_secret = self.oauth_token_secret
+    end
+
+    client.update!(text)
+
+  end
 end
