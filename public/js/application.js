9ae9b121fe505a4023103fbf14408dca66a235d1
codeamac02 <codeamac02@gmail.com>
2015-07-09 20:29:38 -0500

    Returns jub status to the user by AJAX

--- public/js/application.js
+++ public/js/application.js
@@ -5,11 +5,62 @@ $(document).ready(function() {
 		$('#submit').prop('disabled', true);
 		$('#alert h4').text("se esta procesando su petición.");
 		var value = $('textarea').val();
-		$.post("/tweet_ajax", { textarea: value }, function(data){
-			 $('#alert h4').text(data);
+		$.post("/tweet_ajax", { textarea: value }, function(jid){
+			// $('#alert h4').text(jid);
+			console.log(jid);
+			tweetStatus(jid);
+
+
+
+
+
+
+
+			// $.get("/status/" + data, function(status){
+			//  	if (status == "true"){
+			//  		$('#alert h4').text("Tu tweet fue enviado");
+			//  	} else {
+			//  		// $('#alert h4').text("Tu tweet NO fue enviado");
+			//  		setTimeout(function(){
+
+			//  		}, 2000);
+			//  	}
+			// });
 		});
 
 	});
 
 });
+
+
+
+
+
+
+
+
+var count = 0;
+
+
+function tweetStatus(jid) {
+	$.get("/status/" + jid, function(status){
+		console.log(status);
+	 	if (status == "true"){
+	 		$('#alert h4').text("Tu tweet fue enviado");
+	 	} else {
+	 		count = count + 1;
+	 		console.log("jid" + count);
+	 		// $('#alert h4').text("Tu tweet NO fue enviado");
+	 		setTimeout(function(){
+	 			tweetStatus(jid);
+	 		}, 2000);
+	 	}
+	});
+}
+
+
+
+
+
+
 
